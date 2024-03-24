a6918a9a9710802f999059fd6d54e9d8fbe92e56
Hồng Nhung <145759907+NunNunIT@users.noreply.github.com>
2024-03-25 04:36:15 +0700

    Update oAuth.tsx

--- client/src/components/auth/oAuth/oAuth.tsx
+++ client/src/components/auth/oAuth/oAuth.tsx
@@ -16,7 +16,7 @@ export default function OAuth() {
 
         const result = await signInWithPopup(auth, provider);
         console.log(result)
-        const res = await fetch (BACKEND_URL + "/auth/login/google", { 
+        const res = await fetch ("/api/auth/login/google", { 
             method: "POST", 
             headers: {
                 'Content-Type': 'application/json'
